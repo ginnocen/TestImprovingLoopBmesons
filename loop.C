7b1f2d51ec9b6caf0225a6a0827fc33fda619fab
ginnocen <ginnocen@cern.ch>
2014-07-15 16:16:47 +0200

    fix output name

--- loop.C
+++ loop.C
@@ -695,7 +695,7 @@ int signalGen(int Btype, int j)
 
 //void loop(string infile="/export/d00/scratch/jwang/Bfinder_BoostedMC_20140418_Hijing_PPb502_MinimumBias_HIJINGemb_inclBtoPsiMuMu_5TeV.root", string outfile="/export/d00/scratch/jwang/jpsi.root", bool REAL=0){
 void loop(string infile="/afs/cern.ch/work/g/ginnocen/hckim-HIJINGemb_inclBtoPsiMuMu/Bfinder_all_151_1_Y7s.root", string
-	  outfile="Modified/testModified.root", bool REAL=0,bool PbpMC=0,int nEntries=0){
+	  outfile="Original/testOriginal.root", bool REAL=0,bool PbpMC=0,int nEntries=0){
 //////////////////////////////////////////////////////////Phi
 //   This file has been automatically generated 
 //     (Thu Nov 21 13:34:42 2013 by ROOT version5.27/06b)
